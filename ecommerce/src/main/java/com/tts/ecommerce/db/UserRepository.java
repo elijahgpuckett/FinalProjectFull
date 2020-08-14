5ebac868e83d572e4903a14ee8f8ebde32a9c486
elijahgpuckett <elijahgabrielp@gmail.com>
2020-08-14 10:45:17 -0400

    database working

--- ecommerce/src/main/java/com/tts/ecommerce/db/UserRepository.java
+++ ecommerce/src/main/java/com/tts/ecommerce/db/UserRepository.java
@@ -1,8 +1,10 @@
 package com.tts.ecommerce.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tts.ecommerce.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
